5aa700a71fb0fa4a724dac536fc41e9de55f8121
monia <moniatec@gmail.com>
2020-10-13 10:17:02 -0400

    create Home component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './components/Login';
 import Logout from './components/Logout';
+import Home from './components/Home';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Switch>
           <Route exact path="/login" component={Login} />
           <Route exact path="/logout" component={Logout} />
+          <Route exact path="/home" component={Home} />
         </Switch>
       </BrowserRouter>
     </>
