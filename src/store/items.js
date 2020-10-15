68981edc84d85381badef10d0a7d2aab9ebcfc4f
monia <moniatec@gmail.com>
2020-10-15 16:08:23 -0400

    add getOneItem reducer

--- src/store/items.js
+++ src/store/items.js
@@ -6,7 +6,7 @@ const DEL_ITEM = "SHOPPY/items/DEL_ITEM"
 const UPDATE_ITEM = "SHOPPY/items/UPDATE_ITEM"
 
 export const items = (list) => ({ type: HOME_ITEMS, list });
-
+export const getItem = (resItem) => ({ type: ITEM, resItem });
 //send get req to get all items 
 export const getHomeItems = () => async (dispatch, getState) => {
     const {
@@ -24,6 +24,26 @@ export const getHomeItems = () => async (dispatch, getState) => {
     }
 }
 
+//send get req to get details of an item wiht itemId
+export const getOneItem = (itemId) => async (dispatch, getState) => {
+    const {
+        authentication: { token },
+    } = getState();
+
+    const res = await fetch(`${apiBaseUrl}/items/${itemId}`, {
+        // headers: {
+        //     Authorization: `Bearer ${token}`,
+        // },
+    });
+
+    if (res.ok) {
+        const resItem = await res.json();
+
+        dispatch(getItem(resItem))
+
+    }
+}
+
 export default function reducer(state = { list: [] }, action) {
     switch (action.type) {
         case HOME_ITEMS: {
@@ -31,6 +51,14 @@ export default function reducer(state = { list: [] }, action) {
                 ...state,
                 list: action.list
             }
+        }
+        case ITEM: {
+            const newState = Object.assign({}, state)
+            newState.resItem = {
+                item: action.resItem.item,
+
+            }
+            return newState
         }
         default:
             return state;
