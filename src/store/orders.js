1a4e7f0d1995e24884248f0fe774af359b73b118
monia <moniatec@gmail.com>
2020-11-04 11:57:47 -0500

    add post req to set new order

--- src/store/orders.js
+++ src/store/orders.js
@@ -5,7 +5,10 @@ const ORDER = "SHOPPY/orders/ORDER"
 const SET_ORDER = "SHOPPY/orders/SET_ORDER"
 const DEL_ORDER = "SHOPPY/orders/DEL_ORDER"
 const UPDATE_ORDER = "SHOPPY/orders/UPDATE_ORDER"
+
 export const orders = (list) => ({ type: ORDERS, list });
+export const getOrder = (resOrder) => ({ type: ORDER, resOrder });
+export const setOrder = (order) => ({ type: SET_ORDER, order });
 
 //send get req to get all order 
 export const getOrders = () => async (dispatch, getState) => {
@@ -24,6 +27,43 @@ export const getOrders = () => async (dispatch, getState) => {
     }
 }
 
+//send get req to get details of an order wiht orderId
+export const getOneOrder = (orderId) => async (dispatch, getState) => {
+    // console.log('here')
+    const {
+        authentication: { token },
+    } = getState();
+
+    const res = await fetch(`${apiBaseUrl}/orders/${orderId}`, {
+
+    });
+
+    if (res.ok) {
+        const resOrder = await res.json();
+        // console.log(resOrder)
+        dispatch(getOrder(resOrder))
+
+    }
+}
+
+//send post req to create new order 
+export const createOrder = (userId, itemId) => async (dispatch, getState) => {
+    // const userId = window.localStorage.getItem("currentUserId");
+    const res = await fetch(`${apiBaseUrl}/orders`, {
+        method: "post",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ userId, itemId }),
+    });
+
+    if (res.ok) {
+        const { order } = await res.json();
+        // window.location.href = window.location.href;
+
+        dispatch(setOrder(order));
+
+    }
+};
+
 export default function reducer(state = { list: [], order: {} }, action) {
     switch (action.type) {
         case ORDERS: {
@@ -37,6 +77,16 @@ export default function reducer(state = { list: [], order: {} }, action) {
                 ...state,
                 order: action.resOrder.order
             }
+        }
+        case SET_ORDER: {
+            return {
+                ...state,
+                list: [
+                    ...state.list,
+                    action.order
+                ],
+                setOrder: action.order
+            };
         }
         default:
             return state;
